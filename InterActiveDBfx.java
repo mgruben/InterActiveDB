ed318c52c451aa6d70d1d649af998edfc1f07703
mgruben <GrubenM@gmail.com>
2016-08-04 18:38:38 -0500

    Remove as-yet unused imports

--- InterActiveDBfx.java
+++ InterActiveDBfx.java
@@ -17,14 +17,10 @@
 
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
