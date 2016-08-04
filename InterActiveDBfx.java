f2b50610fe9cc02c6478fd91bed192e335a63ca1
mgruben <GrubenM@gmail.com>
2016-08-04 18:54:53 -0500

    Add additional dummy values to table

--- InterActiveDBfx.java
+++ InterActiveDBfx.java
@@ -45,6 +45,8 @@ public class InterActiveDBfx extends Application {
         tv.getColumns().add(col);
         
         tv.getItems().add("Ram");
+        tv.getItems().add("Joe");
+        tv.getItems().add("Ace");
         
         
         Scene sc = new Scene(bp,400,200);
